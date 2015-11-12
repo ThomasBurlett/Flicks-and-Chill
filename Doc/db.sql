17542582bd2883dff2a38648913b72797a694bc5
burlette <burlette@miamioh.edu>
2015-11-12 13:35:16 -0500

    Updated sql db

--- Doc/db.sql
+++ Doc/db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost:3306
--- Generation Time: Nov 12, 2015 at 05:10 PM
+-- Generation Time: Nov 12, 2015 at 07:34 PM
 -- Server version: 5.5.42
 -- PHP Version: 5.6.10
 
@@ -56,3 +56,38 @@ INSERT INTO `movies` (`title`, `imageURL`, `description`, `genre`, `year`, `rati
 ('The Imitation Game', 'http://ia.media-imdb.com/images/M/MV5BNDkwNTEyMzkzNl5BMl5BanBnXkFtZTgwNTAwNzk3MjE@._V1_SY317_CR0,0,214,317_AL_.jpg', 'During World War II, mathematician Alan Turing tries to crack the enigma code with help from fellow mathematicians.\r\n', 'War', 2014, 8, 7),
 ('American Sniper', 'http://ia.media-imdb.com/images/M/MV5BMTkxNzI3ODI4Nl5BMl5BanBnXkFtZTgwMjkwMjY4MjE@._V1_SX214_AL_.jpg', 'Navy S.E.A.L. sniper Chris Kyle''s pinpoint accuracy saves countless lives on the battlefield and turns him into a legend. Back home to his wife and kids after four tours of duty, however, Chris finds that it is the war he can''t leave behind.\r\n', 'War', 2014, 7, 18),
 ('Interstellar', 'http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX214_AL_.jpg', 'A team of explorers travel through a wormhole in space in an attempt to ensure humanity''s survival.', 'Action', 2014, 9, 12);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `suggestions`
+--
+
+CREATE TABLE `suggestions` (
+  `title` text NOT NULL,
+  `imageURL` text NOT NULL,
+  `description` text NOT NULL,
+  `genre` text NOT NULL,
+  `year` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `price` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `username` text NOT NULL,
+  `password` text NOT NULL,
+  `accountType` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`username`, `password`, `accountType`) VALUES
+('admin', 'admin', 2);
